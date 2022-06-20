d53230a0676d96fd2594b81057b418a9df166bdc
alexis <asimpson428@gmail.com>
2022-06-20 16:29:13 -0400

    first commit

---
+++ Untitled.sql
@@ -0,0 +1,13 @@
+SELECT * FROM customers;
+SELECT county_region FROM customers;
+SELECT * FROM customers WHERE id='BL';
+SELECT * FROM customers WHERE id='1-100';
+SELECT * FROM customers WHERE zip_postal_code='1010, 3012, 12209, 05023';
+SELECT * FROM customers WHERE shipRegion='null';
+SELECT * FROM country_region;
+SELECT * FROM city;
+INSERT customer;
+UPDATE * shipRegion to "EuroZone";
+DELETE * FROM order details WHERE id='1';
+
+ 
